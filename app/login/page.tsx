413b15ad4d914a784d587e1945fdc5e365181274
Beka Dugladze <125121125+BekaDugladze@users.noreply.github.com>
2025-09-14 16:56:30 +0400

    Delete app directory

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import Login from '@/components/login/login'
-
-export default function(){
-    return(
-        <section style={{background:'#2b4a23', height: '100vh'}}>
-            <Login />
-        </section>
-    )
-}
